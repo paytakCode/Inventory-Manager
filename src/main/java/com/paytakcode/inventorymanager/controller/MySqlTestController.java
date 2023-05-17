9dd7d616c6d43fb78146db8da4a30ac45c2ac55b
paytakcode <paytakcode@gmail.com>
2023-05-17 16:49:27 +0900

    Test MySql 연동 Test 코드 Http 메소드 수정

--- src/main/java/com/paytakcode/inventorymanager/controller/MySqlTestController.java
+++ src/main/java/com/paytakcode/inventorymanager/controller/MySqlTestController.java
@@ -1,17 +1,18 @@
 package com.paytakcode.inventorymanager.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.paytakcode.inventorymanager.data.dto.MySqlTestDto;
 import com.paytakcode.inventorymanager.service.MySqlTestService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * MySql 연동 테스트를 위한 Controller입니다.
  * @Author 김태산
- * @Version 0.0.1
+ * @Version 0.0.2
  * @Since 2023-05-17
  */
 @RestController
@@ -22,11 +23,10 @@ public class MySqlTestController {
 
     /**
      * Test Table에 Url로 받은 name을 저장하는 서비스를 호출하는 메서드입니다.
-     * 연결 여부만 확인하기 위해 GetMapping을 사용하였습니다.
      * @param name 저장할 이름
      * @return 저장된 MySqlTestDto 정보
      */
-    @GetMapping("/mysql-test")
+    @PostMapping("/mysql-test")
     public String mySqlTestInsert(@RequestParam String name){
         MySqlTestDto insertedMySqlTestDto = mySqlTestService.saveMySqlTest(name);
         return insertedMySqlTestDto.toString();
