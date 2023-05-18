6206189f0fca5e507fe8d839f9b61f4165fa5db9
paytakcode <paytakcode@gmail.com>
2023-05-18 17:56:21 +0900

    Feat Spring Security 적용중

--- src/main/java/com/paytakcode/inventorymanager/api/v1/controller/pageController.java
+++ src/main/java/com/paytakcode/inventorymanager/api/v1/controller/pageController.java
@@ -5,18 +5,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Page 이동을 위한 Controller
+ * PageController
  * @Author 김태산
  * @Version 0.1.0
- * @Since 2023-05-17 오후 11:58
+ * @Since 2023-05-18 오후 1:27
  */
 
 @Controller
 @RequestMapping("/api/v1")
-public class PageController {
+public class pageController {
+
+	@GetMapping("/main")
+	public String mainPage() {
+		return "main";
+	}
+
+	@GetMapping("/intro")
+	public String introPage() {
+		return "intro";
+	}
 
 	@GetMapping("/login")
 	public String loginPage() {
 		return "login";
+	}
+
+	@GetMapping("/signup")
+	public String signupPage() {
+		return "signup";
 	}
 }
