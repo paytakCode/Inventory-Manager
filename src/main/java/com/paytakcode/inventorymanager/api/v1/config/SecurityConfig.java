fb0d0a7bc4c018e80b1fed373865837db528949d
paytakcode <paytakcode@gmail.com>
2023-05-31 09:39:44 +0900

    Feat Cors Expose 'Authorization'
    1.React에서 JWT 값 접근을 위해 설정 추가

--- src/main/java/com/paytakcode/inventorymanager/api/v1/config/SecurityConfig.java
+++ src/main/java/com/paytakcode/inventorymanager/api/v1/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.paytakcode.inventorymanager.api.v1.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * Security Config
@@ -36,6 +37,7 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		CorsConfiguration corsConfig = new CorsConfiguration();
 		corsConfig.addAllowedOrigin("http://localhost:3000");
+		corsConfig.addExposedHeader("Authorization");
 		http
 			.httpBasic().disable()
 			.csrf().disable()
