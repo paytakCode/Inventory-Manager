52181ce8c459506d395f63867a773203ee696c8e
paytakcode <paytakcode@gmail.com>
2023-06-03 16:19:48 +0900

    Feat ApiBaseUrlConfig
    1.기본 URL 설정 통합

---
+++ src/main/java/com/paytakcode/inventorymanager/api/v1/config/ApiBaseUrlConfig.java
@@ -0,0 +1,16 @@
+package com.paytakcode.inventorymanager.api.v1.config;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * API Base URL Config
+ * @Author 김태산
+ * @Version 0.1.0
+ * @Since 2023-06-03 오후 3:51
+ */
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiBaseUrlConfig {
+	public static final String API_BASE_URL = "/api/v1";
+}
