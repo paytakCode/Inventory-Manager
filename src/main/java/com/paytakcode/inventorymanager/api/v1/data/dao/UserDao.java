010401285a216a468e3b03514e6c3be71677f284
paytakcode <paytakcode@gmail.com>
2023-06-17 22:14:08 +0900

    Chore UserDao getUserReferenceByEmail 메소드 제거

--- src/main/java/com/paytakcode/inventorymanager/api/v1/data/dao/UserDao.java
+++ src/main/java/com/paytakcode/inventorymanager/api/v1/data/dao/UserDao.java
@@ -8,7 +8,7 @@ import com.paytakcode.inventorymanager.api.v1.data.entity.UserEntity;
 /**
  * User DAO
  * @Author 김태산
- * @Version 0.2.1
+ * @Version 0.2.2
  * @Since 2023-05-18 오후 3:45
  */
 public interface UserDao {
@@ -19,8 +19,6 @@ public interface UserDao {
 	Optional<UserEntity> findUserById(Long userId);
 
 	Optional<UserEntity> findUserByEmail(String email);
-
-	UserEntity getUserReferenceByEmail(String email);
 
 	UserEntity getUserReferenceById(Long userId);
 }
